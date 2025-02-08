50e25d7f4eea4b06110af18c79319fb592092438
Douglass Kirkley <dkirkley@eitccorp.com>
2025-02-08 14:35:29 -0500

    remove Makefile

--- exporter/trinoexporter/exporter_logs.go
+++ exporter/trinoexporter/exporter_logs.go
@@ -1,6 +1,3 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
 package trinoexporter // import "github.com/dougkirkley/opentelemetry-collector-trino/exporter/trinoexporter"
 
 import (
